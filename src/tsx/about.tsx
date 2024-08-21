ddba300834d598db42ba66293171864204c57239
Oluwaseeni Ajayi <42158213+the-blaque@users.noreply.github.com>
2024-08-20 22:22:34 -0600

    excel photo

--- src/tsx/about.tsx
+++ src/tsx/about.tsx
@@ -8,7 +8,8 @@ import Rukevwe from "../assets/PHOTO-r.jpg";
 import Trybe from "../assets/image8.jpeg";
 import Victor from "../assets/image10.jpeg";
 import Toyin from "../assets/image9.jpeg";
-import Ayo from "../assets/ayo.jpg"
+import Ayo from "../assets/ayo.jpg";
+import excel from "../assets/PHOTO-2024-08-19-19-24-49.jpg";
 
 const About: React.FC = () => {
   return (
@@ -16,46 +17,46 @@ const About: React.FC = () => {
       <div className="flex flex-col min-h-screen">
         <div className="bg-primary text-primary-foreground py-12 md:py-16 lg:py-20">
           <div className="flex flex-col lg:grid lg:grid-cols-2 gap-2">
-          <div className=" justify-center items-center">
-          <iframe
-              src="https://player.vimeo.com/video/993008930?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=0"
-              allow="autoplay"
-              title="Tehillah Teaser"
-            ></iframe>
-          </div>
-          <div className="container px-4 md:px-6 text-left space-y-4">
-            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold tracking-tight">
-              Experience the Power of Praise
-            </h1>
-            <p className="text-lg md:text-xl lg:text-2xl max-w-[800px] mx-auto">
-              More Than Just a Concert
-            </p>
-            <p className="text-base md:text-lg lg:text-xl text-muted-foreground max-w-[700px] mx-auto">
-              Join David's Trybe for an uplifting worship experience that goes
-              beyond music to touch the heart and soul. Experience captivating
-              gospel melodies and powerful performances that inspire hope and
-              joy.
-            </p>
-            <div className="flex flex-col sm:flex-row items-center justify-start gap-2">
-              <a
-                href="https://www.eventbrite.com/e/tehillah-30-tickets-978672554957?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="bg-indigo-900 text-white font-bold py-2 px-4 rounded">
-                  Reserve Your Spot
-                </button>
-              </a>
-              <a
-                href="https://www.youtube.com/@freedomhouserccg/search?query=tehillah"
-                className="inline-flex items-center gap-2 text-muted-foreground hover:underline text-sm"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Watch Past Concerts →
-              </a>
+            <div className=" justify-center items-center">
+              <iframe
+                src="https://player.vimeo.com/video/993008930?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=0"
+                allow="autoplay"
+                title="Tehillah Teaser"
+              ></iframe>
             </div>
-          </div>
+            <div className="container px-4 md:px-6 text-left space-y-4">
+              <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold tracking-tight">
+                Experience the Power of Praise
+              </h1>
+              <p className="text-lg md:text-xl lg:text-2xl max-w-[800px] mx-auto">
+                More Than Just a Concert
+              </p>
+              <p className="text-base md:text-lg lg:text-xl text-muted-foreground max-w-[700px] mx-auto">
+                Join David's Trybe for an uplifting worship experience that goes
+                beyond music to touch the heart and soul. Experience captivating
+                gospel melodies and powerful performances that inspire hope and
+                joy.
+              </p>
+              <div className="flex flex-col sm:flex-row items-center justify-start gap-2">
+                <a
+                  href="https://www.eventbrite.com/e/tehillah-30-tickets-978672554957?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="bg-indigo-900 text-white font-bold py-2 px-4 rounded">
+                    Reserve Your Spot
+                  </button>
+                </a>
+                <a
+                  href="https://www.youtube.com/@freedomhouserccg/search?query=tehillah"
+                  className="inline-flex items-center gap-2 text-muted-foreground hover:underline text-sm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Past Concerts →
+                </a>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -129,7 +130,7 @@ const About: React.FC = () => {
                 Learn More →
               </a></span> */}
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-center justify-center">
-              <div className="bg-card rounded-lg overflow-hidden shadow-lg">
+                <div className="bg-card rounded-lg overflow-hidden shadow-lg">
                   <img
                     src={deb}
                     width={400}
@@ -186,7 +187,7 @@ const About: React.FC = () => {
                     <p className="text-muted-foreground">U.K</p>
                   </div>
                 </div>
-  
+
                 <div className="bg-card rounded-lg overflow-hidden shadow-lg">
                   <img
                     src={dan}
@@ -228,9 +229,6 @@ const About: React.FC = () => {
                     <p className="text-muted-foreground">USA</p>
                   </div>
                 </div>
-                
-
-
 
                 <div className="bg-card mx-auto rounded-lg overflow-hidden shadow-lg items-center justify-center">
                   <img
@@ -246,8 +244,6 @@ const About: React.FC = () => {
                     <p className="text-muted-foreground">Lethbridge</p>
                   </div>
                 </div>
-
-                
 
                 <div className="bg-card rounded-lg overflow-hidden shadow-lg items-center justify-center">
                   <img
@@ -290,22 +286,29 @@ const About: React.FC = () => {
                     <p className="text-muted-foreground">Calgary</p>
                   </div>
                 </div>
-
-                <div className="p-4 space-y-2">
-                    <h3 className="text-xl font-bold">Newlife Voices</h3>
-                    <p className="text-muted-foreground">Lethbridge</p>
-                  </div>
-
-                  <div className="p-4 space-y-2">
-                    <h3 className="text-xl font-bold">John & Sounds of Praise</h3>
-                    <p className="text-muted-foreground">Edmonton</p>
-                  </div>
-
+                <div className="bg-card rounded-lg overflow-hidden shadow-lg items-center justify-center">
+                  <img
+                    src={excel}
+                    width={400}
+                    height={400}
+                    alt="Excel Voices"
+                    className="w-full h-48 object-contain"
+                  />
                   <div className="p-4 space-y-2">
                     <h3 className="text-xl font-bold">Excel Voices</h3>
                     <p className="text-muted-foreground">Lethbridge</p>
                   </div>
+                </div>
 
+                <div className="p-4 space-y-2">
+                  <h3 className="text-xl font-bold">Newlife Voices</h3>
+                  <p className="text-muted-foreground">Lethbridge</p>
+                </div>
+
+                <div className="p-4 space-y-2">
+                  <h3 className="text-xl font-bold">John & Sounds of Praise</h3>
+                  <p className="text-muted-foreground">Edmonton</p>
+                </div>
               </div>
             </div>
           </section>
